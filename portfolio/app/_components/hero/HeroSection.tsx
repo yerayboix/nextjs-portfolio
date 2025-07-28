8f1bbd10c180b6bf48c3f032d9de1e243844a115
Yeray Boix <yerayboix@gmail.com>
2025-07-28 19:02:58 +0200

    refactor: encapsulado el hero en un componente

---
+++ portfolio/app/_components/hero/HeroSection.tsx
@@ -0,0 +1,11 @@
+import DesktopLayout from "./DesktopLayout";
+import MobileLayout from "./MobileLayout";
+
+export default function HeroSection() {
+    return (
+        <>
+            <DesktopLayout />
+            <MobileLayout />
+        </>
+    )
+}
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,11 +1,9 @@
-import DesktopLayout from "./_components/DesktopLayout";
-import MobileLayout from "./_components/MobileLayout";
+import HeroSection from "./_components/hero/HeroSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background">
-      <DesktopLayout />
-      <MobileLayout />
+      <HeroSection />
     </div>
   );
 }
