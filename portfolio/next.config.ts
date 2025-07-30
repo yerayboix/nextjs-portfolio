7a9f9a8d4784ca790e03d48dee31da6f240abd39
Yeray Boix <yerayboix@gmail.com>
2025-07-30 14:29:06 +0200

    feat: ignore eslint during builds

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
       "api.microlink.io", // Microlink Image Preview
     ],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
